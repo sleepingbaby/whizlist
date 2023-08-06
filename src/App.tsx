9c30d90ba45045e8ab24be86ef443a2e377e3ed1
Kelvin Lau <crosby2987@gmail.com>
2023-08-05 23:07:54 -0500

    Landing Page scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
-        <img
-          style={{ width: "400px" }}
-          src="https://img.freepik.com/free-vector/abstract-grunge-style-coming-soon-with-black-splatter_1017-26690.jpg?w=2000&t=st=1688609405~exp=1688610005~hmac=5e3498ee07a167123563de6961b1bd1cda474039194c50b9bc38a55bb5ce4d76"
-          alt="coming soon"
-        ></img>
-      </div>
+      <LandingPage />
     </>
   );
 }
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,46 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+import AppIcon from "./AppIcon";
+
+const LandingPage = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      sx={{ width: "100%" }}
+    >
+      <AppIcon />
+      <Stack direction="row" spacing={2} mt={4} sx={{ width: "50%" }}>
+        <TextField
+          id="filled-basic"
+          label="Location..."
+          variant="filled"
+          sx={{ ...InputCSS }}
+        />
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#597B91",
+            "&:hover": { backgroundColor: "#324754" },
+          }}
+        >
+          Submit
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
+
+const InputCSS = {
+  width: "100%",
+  borderRadius: "8px",
+  backgroundColor: "#D9D9D9",
+  color: "#828282",
+  marginTop: "25px",
+  "& label.Mui-focused": { color: "#828282" },
+  "& .MuiFilledInput-underline:after": {
+    borderBottom: "none",
+  },
+};
+
+export default LandingPage;
