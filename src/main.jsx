7ad65f5d7b2a4f93e199e7ba72612d208271f437
Kelvin Lau <crosby2987@gmail.com>
2023-08-16 21:19:43 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import theme from "./theme";
 import { UserContextProvider } from "./contexts/UserContext";
 import { ListingContextProvider } from "./contexts/ListingContext";
 import { LoadScript } from "@react-google-maps/api";
-
-// const apiKey = "AIzaSyA682ACq2gR8Tu0cgST4rivkjRAF_uX9_I";
 const libraries = ["places"];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
