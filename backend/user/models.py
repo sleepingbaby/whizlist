220431fc33bb1af9f900e0d3617804730fe702e7
Kelvin Lau <crosby2987@gmail.com>
2023-08-23 22:06:29 -0500

    Update backend

--- backend/user/models.py
+++ backend/user/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import AbstractUser
 class App_user(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
-    display_name = models.CharField(unique=True)
-    first_name = models.CharField()
-    last_name = models.CharField()
+    display_name = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     profile_pic = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
     REQUIRED_FIELDS = []
